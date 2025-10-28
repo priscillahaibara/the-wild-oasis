c03af2c09b1ee7ed893155bf1d7b01160d2da678
Priscilla Haibara <megumi.ufrn@gmail.com>
2025-10-28 08:51:28 -0300

    feat: setup initial routes, global styles and basic components (button, input, row, heading)

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
 
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
